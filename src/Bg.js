b976158e9a5e7e15b824b171662e7297db807261
Milana <milaana1988@gmail.com>
2023-09-05 16:12:09 +0300

    header span added

---
+++ src/Bg.js
@@ -0,0 +1,14 @@
+import './Bg.css';
+
+function Bg() {
+  return (
+    <div className="Bg">
+      <div className="header">
+        <span className='header_text'>Please upload a picture for background removal</span>
+        <span className='header_btn'>Upload photo</span>
+      </div>
+    </div>
+  );
+}
+
+export default Bg;
